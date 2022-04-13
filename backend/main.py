4bf88f4a54d9d7c7e48656e902571ce5dd81c232
Renan Santana <renan_ota@hotmail.com>
2022-04-12 22:48:17 -0300

    implementa lógica do jogo

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,4 @@
+from urllib import response
 from fastapi import FastAPI
 
 from models import Guess, Word
@@ -15,7 +16,20 @@ def read_root():
 
 @app.post("/check")
 def check_guess(guess: Guess):
-    word = guess.word
-    secret_world = Word.get()
-    print(secret_world.word)
-    return word
+    guess_word = guess.word
+    query = Word.get()
+    secret_world = query.word
+    response = []
+
+    if len(guess_word) != len(secret_world):
+        return {"message": "wrong number of letters"}
+
+    for guess_letter, correct_letter in zip(guess_word, secret_world):
+        if guess_letter == correct_letter:
+            response.append(1)
+        elif guess_letter in secret_world:
+            response.append(2)
+        else:
+            response.append(0)
+
+    return {"result": response}
