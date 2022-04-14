7165fb1eeec4c904a34534d159dccb9cec25bbce
Renan Santana <renan_ota@hotmail.com>
2022-04-14 00:33:56 -0300

    setup base app

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <b-container fluid>
+      <b-row>
+        <b-col>
+          <b-navbar type="dark" variant="info">
+            <b-navbar-brand href="#">Termo Clone</b-navbar-brand>
+          </b-navbar>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col>
+          <b-card title="Palavra do dia:">
+            <b-form>
+              <b-row>
+                <b-col>
+                  <b-form-input></b-form-input>
+                </b-col>
+                <b-col>
+                  <b-form-input></b-form-input>
+                </b-col>
+                <b-col>
+                  <b-form-input></b-form-input>
+                </b-col>
+                <b-col>
+                  <b-form-input></b-form-input>
+                </b-col>
+                <b-col>
+                  <b-form-input></b-form-input>
+                </b-col>
+              </b-row>
+              <br>
+              <b-row>
+                <b-col>
+                  <b-button block type="submit" variant="success">
+                    Enviar
+                  </b-button>
+                </b-col>
+              </b-row>
+            </b-form>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).mount('#app')
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
