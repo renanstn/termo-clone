9630d3bdff522151b29da0ff332c52819597a261
Renan Santana <renan_ota@hotmail.com>
2022-04-15 05:26:58 -0300

    setup base app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,13 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
