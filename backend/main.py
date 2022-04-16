6e29f07d93c778efb5ed1735f32e356692cd7eca
Renan Santana <renan_ota@hotmail.com>
2022-04-16 05:23:45 -0300

    add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from models import Guess, Word
 from database import init_database
+import settings
 
 
 init_database()
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -13,20 +22,27 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.get("/letter_count")
+def letter_cont():
+    query = Word.get()
+    secret_word = query.word
+    return {"letters": len(secret_word)}
+
+
 @app.post("/check")
 def check_guess(guess: Guess):
     guess_word = guess.word
     query = Word.get()
-    secret_world = query.word
+    secret_word = query.word
     response = []
 
-    if len(guess_word) != len(secret_world):
+    if len(guess_word) != len(secret_word):
         return {"message": "wrong number of letters"}
 
-    for guess_letter, correct_letter in zip(guess_word, secret_world):
+    for guess_letter, correct_letter in zip(guess_word, secret_word):
         if guess_letter == correct_letter:
             response.append(1)
-        elif guess_letter in secret_world:
+        elif guess_letter in secret_word:
             response.append(2)
         else:
             response.append(0)
--- backend/settings.py
+++ backend/settings.py
@@ -4,3 +4,6 @@ from decouple import config
 DATABASE_URL = config(
     "DATABASE_URL", default="postgresql://postgres:postgres@database/termo"
 )
+ORIGINS = [
+    "http://localhost:8080",
+]
