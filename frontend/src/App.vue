62b93fe2a97cffdb16c04c764a5fad133f53b20e
Renan Santana <renan_ota@hotmail.com>
2022-04-16 05:31:40 -0300

    basic layout

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <div class="row">
+      <TopMenu/>
+    </div>
+    <br>
+    <div class="row justify-content-center">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Palavra do dia:</h5>
+            <div class="row">
+              <div class="col" v-for="letter in word_leters" :key="letter">
+                <input class="form-control" type="text" :name="letter">
+              </div>
+            </div>
+            <br>
+            <a href="#" class="btn btn-primary">Enviar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import TopMenu from '@/components/TopMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TopMenu
+  },
+  data() {
+    return {
+      word_leters: 0,
+    }
+  },
+  created() {
+    this.get_number_of_letters()
+  },
+  methods: {
+    get_number_of_letters() {
+      const url = "http://localhost:8000/letter_count"
+      axios.get(url)
+      .then((response) => {
+        console.log(response.data)
+        this.word_leters = response.data.letters
+      })
+    }
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ frontend/src/components/TopMenu.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Termo-Clone</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "TopMenu",
+}
+</script>
